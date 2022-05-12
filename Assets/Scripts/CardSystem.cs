81c4f128c8fce15653b67bc8c4690ac87043e4e4
Reizi_Home <49th.nobody@gmail.com>
2022-05-12 21:59:17 +0300

    работа над PlayerController и CardSystem

--- Assets/Scripts/CardSystem.cs
+++ Assets/Scripts/CardSystem.cs
@@ -1,6 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardSystem : MonoBehaviour
 {
+    public static CardSystem instance;
 
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<Card> StartingDeck { get; private set; }
+    private List<Card> _tradeDeck;
+    private List<CardController> _tradeRow;
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public OutputController Authority;
 
+    public Transform Hand;
+    public CardController CardPrefab;
+    private List<CardController> _hand;
+
+    private List<Card> _deck;
+    private List<Card> _discardPile;
+
+    private void Awake()
+    {
+        _discardPile = new List<Card>();
+        _hand = new List<CardController>();
+    }
+
+    public void GameStart()
+    {
+        Authority.UpdateValue(50);
+        _deck = CardSystem.instance.StartingDeck;
+        Shuffle();
+    }
+
+    private void Shuffle()
+    {
+        var newDeck = new List<Card>();
+        newDeck.AddRange(_deck);
+        newDeck.AddRange(_discardPile);
+        _deck = newDeck;
+        _discardPile.Clear();
+    }
 }
